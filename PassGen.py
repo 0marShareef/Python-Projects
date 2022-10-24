04a05d70ce8ef69b726458afa050f459d21e5a44
0marShareef <89986131+0marShareef@users.noreply.github.com>
2022-10-24 13:53:26 +0530

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -11,8 +11,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-final_range = nr_letters + nr_symbols + nr_numbers
-
 # Creating a list of characters to be used in password - "pass_list":
 pass_list = []
 for i in range(nr_letters):
